753226eb2577cb31050b45e2a1a53c575abfa4c6
rkrohitkakade <kakade2506@gmail.com>
2020-11-24 00:08:49 +0530

    Removed Eureka Client

--- src/main/java/com/tcs/poc/app/DashboardManagementServiceApplication.java
+++ src/main/java/com/tcs/poc/app/DashboardManagementServiceApplication.java
@@ -2,10 +2,9 @@ package com.tcs.poc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DashboardManagementServiceApplication {
 
 	public static void main(String[] args) {
